b930101dd92e4108f34b426ab96823b728bafcbf
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2023-10-02 16:53:31 -0600

    Hola

---
+++ app/src/main/java/com/cartelderamironuevageneracion/gatito_app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.cartelderamironuevageneracion.gatito_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button ini= findViewById(R.id.btnini);
+
+        ini.setOnClickListener(v ->{
+            Intent i = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(i);
+        });
+    }
+}
